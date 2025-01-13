823e216d8c9a550b615a2c5cf604460170214b27
Sakkar Icham <89643646+isakkar@users.noreply.github.com>
2025-01-13 16:41:58 +0100

    Added setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+name='{Game_of_Life-Icham-DIA6}',
+version='1.0',
+author='{Icham_SAKKAR}',
+license='MIT',
+long_description=open('README.md').read(),
+)
